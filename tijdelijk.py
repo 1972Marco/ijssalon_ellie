7c9f7b161dbc7c87639ac0a6e76315e8b4770163
Marco Verberk <marcomcmlxxii@gmail.com>
2023-07-19 18:42:06 +0200

    Les 7 opdracht 7

--- tijdelijk.py
+++ tijdelijk.py
@@ -7,7 +7,9 @@ aanbieding = prijzen["vanille"] * 0.8
 reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts €{aanbieding}"
 reclame_tekst2 = reclame_tekst[:62] # aanbieding heeft de waarde 3.2 en heeft geen nullen achter de komma gegeven bij mij.
 reclame_tekst3 = reclame_tekst2.upper()
-print(reclame_tekst3)
+reclame_tekst4 = reclame_tekst3.split(" ")
+print(reclame_tekst4)
+
 
 
 
