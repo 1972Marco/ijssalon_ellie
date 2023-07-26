31c9888583e5a01dd6ad17023778ffc069f2ae5b
Marco Verberk <marcomcmlxxii@gmail.com>
2023-07-26 22:20:00 +0200

    Huiswerk les 10

--- helper.py
+++ helper.py
@@ -1,10 +1,12 @@
 def decoreer(tekst=""):
+    tekst="header"
     lengte = len(tekst) + 4
     print()
     print(lengte * "*")
     print(f"* {tekst} *")
     print(lengte * "*")
     print()
+
 def fooi_pp(bedrag,personen):
     try:
         bedrag_pp = bedrag / personen
@@ -12,9 +14,28 @@ def fooi_pp(bedrag,personen):
         bedrag_pp ="??"
     return f"Het bedrag per persoon is {bedrag_pp} euro."
 
+def onderstreep(tekst=""):
+    uit =[]
+    uit.append(tekst)
+    uit.append(len(tekst) * "=")
+    return uit
+
+def som(inkomsten):
+    totaal = sum(inkomsten.values())
+    return totaal
+
+
+
+
+
+
+
+
+
+'''
 b = int(input("Welk berag zit er in de fooienpot? "))
 p = int(input("Over hoeveel mensen moet de pot verdeeld worden? "))
 
 print (fooi_pp(b,p))
-
+'''
        
---
+++ presentatie.py
@@ -0,0 +1,11 @@
+def presenteer(inkomsten, totaal_inkomsten):
+    for k, v in inkomsten.items():
+        sleutel = k
+        waarde = v
+        print(f"{sleutel} : {waarde} euro")
+            
+    print("==========================")
+    print(f"totaal : {totaal_inkomsten} euro")
+
+
+    
